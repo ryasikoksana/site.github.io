91cfcd0ea5efe3630ef8a2e2f9aec1899bc6e867
Ryasik Oksana <ryasik.oksana@yandex.ru>
2018-01-27 23:34:00 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+var slides = document.querySelectorAll('#slides .slide');
+			var currentSlide = 0;
+			var slideInterval = setInterval(nextSlide,5000);
+ 
+			function nextSlide() {
+				goToSlide(currentSlide+1);
+			}
+ 
+			function previousSlide() {
+				goToSlide(currentSlide-1);
+			}
+
+			function goToSlide(n) {
+				slides[currentSlide].className = 'slide';
+				currentSlide = (n+slides.length)%slides.length;
+				slides[currentSlide].className = 'slide showing';
+			}
+			
+			var playing = true;
+			var pauseButton = document.getElementById('pause');
+			function pauseSlideshow() {
+				pauseButton.innerHTML = 'Включить';
+				playing = false;
+				clearInterval(slideInterval);
+			}
+ 
+			function playSlideshow() {
+				pauseButton.innerHTML = 'Пауза';
+				playing = true;
+				slideInterval = setInterval(nextSlide,2000);
+			}
+ 
+			pauseButton.onclick = function() {
+				if(playing) {
+					pauseSlideshow();
+				} else {
+					playSlideshow();
+				}
+			};
+			
+			var next = document.getElementById('next');
+			var previous = document.getElementById('previous');
+			next.onclick = function() {
+				pauseSlideshow();
+				nextSlide();
+			};
+				previous.onclick = function() {
+				pauseSlideshow();
+				previousSlide();
+			};
